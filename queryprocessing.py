7bfb3d6d6342c3a01af7c0b1f69c0177a7d5c9bb
lenardrommel <lenard.rommel@student.uni-tuebingen.de>
2024-06-27 16:07:34 +0200

    queryprocessing

--- queryprocessing.py
+++ queryprocessing.py
@@ -2,6 +2,7 @@ import spacy
 import time
 import warnings
 from keybert import KeyBERT
+from yake import KeywordExtractor
 nlp = spacy.load("en_core_web_sm")
 kw_model = KeyBERT('distilbert-base-nli-mean-tokens')
 
@@ -32,7 +33,40 @@ def filter_keywords_KeyBERT(query):
     filtered_keywords = [word for word in all_keywords if any(token.pos_ in ['NOUN', 'PROPN'] for token in nlp(word))]
 
     # Print the final keywords
-    print("Final Keywords:", filtered_keywords)
+    print("Final Keywords KEYBERT:", filtered_keywords)
+
+    return filtered_keywords
+
+
+def filter_keywords_YAKE(query):
+    '''
+        This function takes a query as input and extracts keywords using spaCy and YAKE.
+        :param query: a string query longer than 3 characters.
+        :return: list of keywords extracted from the query.
+        '''
+    assert isinstance(query, str), "Input query must be a string."
+    assert len(query) > 0, "Input query must not be empty."
+    if len(query) < 4:
+        return [query]
+
+    doc = nlp(query)
+
+    spacy_keywords = [ent.text for ent in doc.ents]
+    noun_chunks = [chunk.text for chunk in doc.noun_chunks]
+
+    # Extract keywords with YAKE
+    yake_extractor = KeywordExtractor(lan="en", n=2, dedupLim=0.9, top=10, features=None)
+    yake_keywords = yake_extractor.extract_keywords(query)
+    yake_keywords = [kw[0] for kw in yake_keywords]
+
+    # Combine, deduplicate, and filter keywords
+    all_keywords = set(spacy_keywords + noun_chunks + yake_keywords)
+
+    # Further filter keywords to focus on nouns and proper nouns
+    filtered_keywords = [word for word in all_keywords if any(token.pos_ in ['NOUN', 'PROPN'] for token in nlp(word))]
+
+    # Print the final keywords
+    print("Final Keywords YAKE:", filtered_keywords)
 
     return filtered_keywords
 
@@ -49,6 +83,7 @@ if __name__ == "__main__":
         start = time.time()
         print(f"Query: {t}")
         filter_keywords_KeyBERT(t)
+        filter_keywords_YAKE(t)
         print("\n")
         print(f'Execution time: {time.time() - start:.2f} seconds')
 
